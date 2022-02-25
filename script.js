9a8a009ca7a8d83cc4ff515c79f1733536c839a2
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-25 20:05:17 +0000

    add functionality and edit styles for navigation

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const iconCloseMenu = document.querySelector(".icon-close-menu");
+const backgroundDim = document.querySelector("#background-dim");
 
 hamburger.addEventListener("click", (e) => {
   navMenu.style.display = "block";
   hamburger.style.display = "none";
   iconCloseMenu.style.display = "block";
+  backgroundDim.classList.add("background-dim");
 });
 
 iconCloseMenu.addEventListener("click", (e) => {
   iconCloseMenu.style.display = "none";
   hamburger.style.display = "block";
   navMenu.style.display = "none";
+  backgroundDim.classList.remove("background-dim");
 });
