2bc26bc076d193ceb6f8a4b47514c6d9bb56a339
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-26 17:00:39 +0000

    fix positioning and styling for mobile nav and main modal

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const iconCloseMenu = document.querySelector(".icon-close-menu");
-const background = document.querySelector("#background");
+const background = document.querySelector(".bg-modal");
+const backgroundNav = document.querySelector(".bg-modal-nav");
 const backProjectBtn = document.querySelector("#back-project-btn");
 const modalMain = document.querySelector(".modal-main");
 const backProjectCloseBtn = document.querySelector(".back-project-close-btn");
@@ -12,26 +13,26 @@ hamburger.addEventListener("click", (e) => {
   navMenu.style.display = "block";
   hamburger.style.display = "none";
   iconCloseMenu.style.display = "block";
-  background.classList.add("background");
+  backgroundNav.style.display = "block";
 });
 
 iconCloseMenu.addEventListener("click", (e) => {
   iconCloseMenu.style.display = "none";
   hamburger.style.display = "block";
   navMenu.style.display = "none";
-  background.classList.remove("background");
+  backgroundNav.style.display = "none";
 });
 
 // back project event listener
 
 backProjectBtn.addEventListener("click", (e) => {
   modalMain.style.display = "block";
-  background.classList.add("background-dim");
   hamburger.classList.add("dim-nav");
+  background.style.display = "block";
 });
 
 backProjectCloseBtn.addEventListener("click", (e) => {
   modalMain.style.display = "none";
-  background.classList.remove("background-dim");
   hamburger.classList.remove("dim-nav");
+  background.style.display = "none";
 });
